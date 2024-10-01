a363efda5b314aa055de312a3dba0373f50bd91f
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-09-30 21:26:48 -0300

    Adds TS and Prisma

--- course-api-design-scott-moss/vanilla/src/server.js
+++ course-api-design-scott-moss/vanilla/src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   console.log("Hello from Express!");
   res.status(200);
   res.json({ message: "Hello from JSON" });
