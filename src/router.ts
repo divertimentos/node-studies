3dc4ae815b7b80c7bad089c00892631243a716e6
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-10-02 21:30:39 -0300

    Creates some routes

---
+++ src/router.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+
+const router = Router();
+
+/* Product  */
+router.get("/product", () => {}); // READ
+router.get("/product/:id", () => {}); // READ
+router.put("/product/:id", () => {}); // UPDATE
+router.post("/product", () => {}); // CREATE
+router.delete("/product/:id", () => {}); // DELETE
+
+/* Update  */
+router.get("/update", () => {}); // READ
+router.get("/update/:id", () => {}); // READ
+router.put("/update/:id", () => {}); // UPDATE
+router.post("/update", () => {}); // CREATE
+router.delete("/update/:id", () => {}); // DELETE
+
+/* Update Point  */
+router.get("/updatepoint", () => {}); // READ
+router.get("/updatepoint/:id", () => {}); // READ
+router.put("/updatepoint/:id", () => {}); // UPDATE
+router.post("/updatepoint", () => {}); // CREATE
+router.delete("/updatepoint/:id", () => {}); // DELETE
